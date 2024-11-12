7f1d85a29c1e9670d930e248f888fb29341a8e81
Antonio Alaniz <antonio.alaniz@gmail.com>
2024-11-11 19:39:47 -0800

    Refactor Bill to namedtuple, PEP8

--- congress/bills.py
+++ congress/bills.py
@@ -1,16 +1,6 @@
 """Module to manage congressional bill data."""
 
-class Bill(object):
-    """An instance of a congressional bill."""
+from collections import namedtuple
 
-    def __init__(self, congress, latest_action, number, origin_chamber, title,
-                 bill_type, update_date, update_including_text, url):
-        self.congress = congress
-        self.latest_action = latest_action
-        self.number = number
-        self.origin_chamber = origin_chamber
-        self.title = title
-        self.bill_type = bill_type
-        self.update_date = update_date
-        self.update_including_text = update_including_text
-        self.url = url
+Bill = namedtuple('Bill', ['congress', 'latest_action', 'number', 'origin_chamber',
+                           'title', 'bill_type', 'update_date', 'update_including_text', 'url'])
